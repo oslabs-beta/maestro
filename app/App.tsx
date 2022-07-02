16f0d6cd26c38b2c287851545d8f26f5f8d8b751
Peter Kennedy <pkennedy97@gmail.com>
2022-07-02 17:28:11 -0400

    added webpack

---
+++ app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ButtonTest from './Components/ButtonTest';
+
+
+function App() {
+    return (
+        // <div>hello2</div>
+        <ButtonTest />
+    );
+}
+
+export default App
+
---
+++ electron/server.ts
@@ -0,0 +1,3 @@
+
+
+const imNotEmpty = ['no im not']
