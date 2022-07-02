53862b43246873d865bb61cdeffd65730fc5d60b
Gandalf <gandalfthewise1@protonmail.com>
2022-07-02 11:38:28 -0500

    new boilerplate for a Typescript React Electron app

---
+++ electron/main.ts
@@ -0,0 +1,24 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+// import electronReload from 'electron-reload';
+// import path from 'path';
+
+// require('electron-reload')(__dirname);
+
+let mainWindow: BrowserWindow;
+
+app.on("ready", createWindow);
+
+function createWindow (): void {
+  mainWindow = new BrowserWindow({
+    width:800,
+    height:700,
+    // show: false,
+    webPreferences:{
+      nodeIntegration: true,
+      contextIsolation: false,
+      // preload: __dirname + "/preload.js"
+  }
+  })
+  mainWindow.loadFile("./app/index.html");
+  mainWindow.on("ready-to-show", () => mainWindow.show());
+}
