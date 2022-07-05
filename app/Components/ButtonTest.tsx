dcf41a84fe2820e53b60717512807c3c513adc2a
Aliya Yano <aliyayano@Aliyas-MBP.lan>
2022-07-05 14:43:37 -0700

    initial setup for backend functions

--- app/Components/ButtonTest.tsx
+++ app/Components/ButtonTest.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+const renderThis= async (): Promise<any> => {
+    // Use IPC API to query Electron's main thread and run this method
+    const result = await window.electron.getAlerts()
+    const result2 = await window.electron.getEvents()
+    const namespace = await window.electron.getNamespace()
+
+    console.log("after1",result)
+    console.log("after2",result2)
+    console.log("after4",namespace)
+  }
 
 const ButtonTest = (): JSX.Element => {
     return (
         <>
-            <button>show me a button</button>
+            <button onClick={renderThis}>show me a button</button>
         </>
     );
 };
--- electron/main.ts
+++ electron/main.ts
@@ -1,8 +1,14 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
+const fetch:any = (...args:any) =>
+  import('node-fetch').then(({ default: fetch }:any) => fetch(...args));
+import * as child_process from 'child_process'; 
+
+const prometheusURL = 'http://127.0.0.1:9090/api/v1/';
+
 // import electronReload from 'electron-reload';
 // import path from 'path';
 
-// require('electron-reload')(__dirname);
+//require('electron-reload')(__dirname);
 
 let mainWindow: BrowserWindow;
 
@@ -15,11 +21,38 @@ function createWindow (): void {
     // show: false,
     webPreferences:{
       nodeIntegration: true,
-      contextIsolation: false,
-      // preload: __dirname + "/preload.js"
+      //contextIsolation: false,
+      preload: __dirname + "/preload.js"
   }
   })
   mainWindow.loadURL('http://localhost:8080/');
-  //mainWindow.loadFile("./app/index.html");
+  mainWindow.webContents.openDevTools()
   mainWindow.on("ready-to-show", () => mainWindow.show());
 }
+
+// closes electron on mac devices
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// TO DO: add error handling to fetch request
+// TO DO: Type data/responses
+//functions 
+ipcMain.handle('getAlerts', async () => {
+  const response = await fetch(`${prometheusURL}/rules`)
+  const data:any = await response.json()
+  const result = data.data.groups
+  return result;  
+})
+
+ipcMain.handle('getEvents', async () => {
+  const response:any =  await child_process.execSync('kubectl get events --all-namespaces',{ encoding: 'utf8'});
+  return response;
+})
+
+ipcMain.handle('getNamespace', async () => {
+  const response:any =  await child_process.execSync('kubectl get namespace', { encoding: 'utf8'});
+  return response;
+})
