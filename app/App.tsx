5687e35c3933ab06cd41c2f81f9dbf0b7dd08c51
Peter Kennedy <pkennedy97@gmail.com>
2022-07-07 15:33:32 -0400

    added summary cards and container as well as some bsaic styling

--- app/App.tsx
+++ app/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ButtonTest from './Components/ButtonTest';
 import Namespace from './Components/namespace/Namespace';
 import AlertsContainer from './Containers/AlertsContainer'
-// import './styles/index.css'
+import SummaryDataContainer from './Containers/SummaryDataContainer';
+import './styles/index.css'
 
 
 function App() {
     return (
         // <div>hello2</div>
         // <Namespace />
-        <AlertsContainer />
+        // <AlertsContainer />
+        <SummaryDataContainer />
     );
 }
 
