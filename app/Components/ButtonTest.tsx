18c990cb7d1b5b24c57a8e988ea93debe4527386
Gandalf <gandalfthewise1@protonmail.com>
2022-07-07 11:21:01 -0500

    Cleaned up the data for getNodeList(), getServices(), getPods(), getDeployments()

--- app/Components/ButtonTest.tsx
+++ app/Components/ButtonTest.tsx
@@ -5,14 +5,18 @@ const renderThis= async (): Promise<any> => {
     const alerts = await window.electron.getAlerts();
     const events = await window.electron.getEvents();
     const namespaces = await window.electron.getNamespaces();
-    // const node = await window.electron.getNodeList();
-    // const services = await window.electron.getServices();
-    // const pods = await window.electron.getPods();
-    // const deployments = await window.electron.getDeployments();
+    const node = await window.electron.getNodeList();
+    const services = await window.electron.getServices();
+    const pods = await window.electron.getPods();
+    const deployments = await window.electron.getDeployments();
 
     console.log("alerts", alerts)
     console.log("events", events)
     console.log("namespaces", namespaces)
+    console.log("node", node)
+    console.log("services", services)
+    console.log("pods", pods)
+    console.log("deployments", deployments)
   }
 
 const ButtonTest = (): JSX.Element => {
