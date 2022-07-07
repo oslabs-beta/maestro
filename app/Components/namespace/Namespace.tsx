78c8abc1d50ea37acb46dfe6beea07706d233784
Peter Kennedy <pkennedy97@gmail.com>
2022-07-07 11:13:24 -0400

    implemented thunk middleware to get namespaces

--- app/Components/namespace/Namespace.tsx
+++ app/Components/namespace/Namespace.tsx
@@ -1,29 +1,23 @@
-import React, { Fragment, useState, FC, useEffect } from 'react';
+import React, { Fragment, useState, FC, useEffect, useCallback } from 'react';
 import {useAppDispatch, useAppSelector} from '../../state/hooks';
-import { setCurrentNamespace, getAllNamespaces } from './namespaceSlice';
+import { setCurrentNamespace } from './namespaceSlice';
+import { getNamespaces } from './namespaceSlice';
 import Select from 'react-select'
 
 
-// const namespaces = useAppSelector(state => state.namespace)
-// const dispatch = useAppDispatch()
-
-// const getSomeNamespaces = async (): Promise<Record<string, number | string>>  => {
-//   let res = await window.electron.getNamespace()
-//   let finalRes = await res
-//   let array = []
-//   array.push(finalRes)
-//   getAllNamespaces(array)
-//   return finalRes
-// }
-
-// const promiseMeThis = async (): Promise<any> => {
-//   const pleaseWork = await getSomeNamespaces()
-// }
-//const receivedData = promiseMeThis()
-
 const Namespace: React.FC = () => {
+    const dispatch = useAppDispatch();
+
+    const initApp = useCallback(async () => {
+      await dispatch(getNamespaces());
+    }, [dispatch]);
+
+    useEffect(() => {
+      initApp();
+    }, [])
   
     const namespaceData = useAppSelector(state => state.namespace.allNamespaces);
+    console.log(namespaceData)
     return (
       <>
         <Select />
--- app/Components/namespace/namespaceSlice.ts
+++ app/Components/namespace/namespaceSlice.ts
@@ -1,32 +1,43 @@
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
+import { response } from 'express';
 
 interface NamespaceState {
     currentNamespace: string,
     allNamespaces: string[]
 }
+
 const initialState: NamespaceState = {
         currentNamespace: '',
         allNamespaces: []
     }
-    
+
+export const getNamespaces = createAsyncThunk<string[]>(
+    'namespace/getNamespaces',
+    async (_, thunkAPI) => {
+        const res = await window.electron.getNamespaces()
+        return res
+    }
+);
 
 export const namespaceSlice = createSlice({
     name: 'namespace',
     initialState,
     reducers: {
-        getAllNamespaces: (state, action: PayloadAction<string[]>) => {
-            state.allNamespaces = action.payload
-        },
+        // getAllNamespaces: (state, action: PayloadAction<string[]>) => {
+        //     state.allNamespaces = action.payload
+        // },
         setCurrentNamespace: (state, action: PayloadAction<string>) => {
             state.currentNamespace = action.payload
         }
     },
-    extraReducers: () => {
-
+    extraReducers: (builder) => {
+        builder.addCase(getNamespaces.fulfilled, (state, action) => {
+            state.allNamespaces = action.payload
+        })
     }
 })
 
 
-export const { getAllNamespaces, setCurrentNamespace } = namespaceSlice.actions
+export const { setCurrentNamespace } = namespaceSlice.actions
 export default namespaceSlice.reducer;
 
