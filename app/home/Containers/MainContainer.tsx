42bbdcb33a55e2bd52916b5688f0fff61808e95c
Peter Kennedy <pkennedy97@gmail.com>
2022-07-15 11:31:42 -0400

    tested routing

--- app/home/Containers/MainContainer.tsx
+++ app/home/Containers/MainContainer.tsx
@@ -1,30 +1,47 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import StatusContainer from "./StatusContainer";
 import EventsContainer from "./EventsContainer";
 import Namespace from "../../Components/namespace/Namespace";
 import SidebarContainer from "../../Containers/SidebarContainer";
 import MetricsContainer from "../../metrics/Container/MetricsContainer";
 
+// const MainContainer = () => {
+//   return (
+//     <Router>
+//       <div className="main-container">
+//         <div className="sidebar-container">
+//           <SidebarContainer />
+//         </div>
+//         <div className="home-container">
+//           <Namespace />
+//           {/* <div className="overview-container">
+//             <StatusContainer />
+//             <EventsContainer />
+//           </div> */}
+//           <div className='metrics-container'>
+//             <MetricsContainer />
+//           </div>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// };
+
 const MainContainer = () => {
   return (
-    <Router>
-      <div className="main-container">
-        <div className="sidebar-container">
-          <SidebarContainer />
-        </div>
-        <div className="home-container">
-          <Namespace />
-          {/* <div className="overview-container">
-            <StatusContainer />
-            <EventsContainer />
-          </div> */}
-          <div className='metrics-container'>
-            <MetricsContainer />
-          </div>
-        </div>
-      </div>
-    </Router>
+    <>
+       <div className="sidebar-container">
+         <SidebarContainer />
+       </div>
+    <Routes>
+        <Route path="/"><Namespace /></Route>
+        <Route path="/home" element={<StatusContainer />}/>
+        <Route path="/home"><EventsContainer /></Route>
+        <Route path ="/metrics"><MetricsContainer /></Route>
+    </Routes>
+    </>
   );
 };
 
