50b0544c3a20faa1429795e20283864894590711
Gandalf <gandalfthewise1@protonmail.com>
2022-07-19 11:52:40 -0500

    Deleted unused MetricsGraphCard.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import ButtonTest from "./ButtonTest";
-import Namespace from "./Components/namespace/Namespace";
-import MainContainer from "./home/Containers/MainContainer";
 import DashboardContainer from "./home/Containers/DashboardContainer";
 import "./styles/index.css";
 
